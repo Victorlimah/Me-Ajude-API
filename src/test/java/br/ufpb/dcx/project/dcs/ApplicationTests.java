26cc85c3d16596ab9922dceca0a715eefdb12795
Carlos <carlos@pop-os.localdomain>
2023-09-16 16:39:13 -0300

    init: spring

---
+++ src/test/java/br/ufpb/dcx/project/dcs/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.project.dcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
