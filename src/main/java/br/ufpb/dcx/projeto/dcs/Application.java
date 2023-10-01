9a60c12df530893c50f9939457b196ff06705aec
Carlos Gabriel <dev.carlos.albuquerque@gmail.com>
2023-10-01 15:55:37 -0300

    chore: update Spring Boot to version 3.1.4

--- src/main/java/br/ufpb/dcx/projeto/dcs/Application.java
+++ src/main/java/br/ufpb/dcx/projeto/dcs/Application.java
@@ -1,4 +1,4 @@
-package br.ufpb.dcx.project.dcs;
+package br.ufpb.dcx.projeto.dcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/ufpb/dcx/projeto/dcs/ApplicationTests.java
+++ src/test/java/br/ufpb/dcx/projeto/dcs/ApplicationTests.java
@@ -1,4 +1,4 @@
-package br.ufpb.dcx.project.dcs;
+package br.ufpb.dcx.projeto.dcs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
