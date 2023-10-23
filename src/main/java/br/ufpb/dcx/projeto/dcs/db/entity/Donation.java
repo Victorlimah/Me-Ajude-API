3ac9bec053e22c91ad8c20bb72eed3b447d268b2
Victor Gabriel <victorgabriel.lima03@gmail.com>
2023-10-23 01:27:16 -0300

    fix: donation entity

--- src/main/java/br/ufpb/dcx/projeto/dcs/db/entity/Donation.java
+++ src/main/java/br/ufpb/dcx/projeto/dcs/db/entity/Donation.java
@@ -1,9 +1,17 @@
 package br.ufpb.dcx.projeto.dcs.db.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "donation")
 public class Donation {
 
     @Id
@@ -12,7 +20,8 @@ public class Donation {
 
     private double amount;
 
-    private LocalDate date = LocalDate.now();
+    @Builder.Default
+    private LocalDate created = LocalDate.now();
 
     @ManyToOne()
     @JoinColumn(name = "user_id")
