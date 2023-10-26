c1ab2d425b5a1bff3d97e5a0645c2a450aa212af
Victor Gabriel <victorgabriel.lima03@gmail.com>
2023-10-26 01:08:22 -0300

    fix: path controller

--- src/main/java/br/ufpb/dcx/projeto/dcs/controller/CampaignSearchController.java
+++ src/main/java/br/ufpb/dcx/projeto/dcs/controller/CampaignSearchController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/search/campaigns")
+@RequestMapping("/v1/api/search/campaigns")
 public class CampaignSearchController {
 
     @Autowired
